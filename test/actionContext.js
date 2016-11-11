0eb3c6699671f306454bf9ca7c731bbde146d002
Yoanm <yoanm@users.noreply.github.com>
2016-11-11 21:44:09 +0100

    Fix

--- test/actionContext.js
+++ test/actionContext.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 const SarahActionContext = require('../actionContext');
 
 describe('sarah.lib.utils/actionContext', function() {
-    it('hold context data', function() {
+    it('hold context data and callback', function() {
         var data = {myData: 'myValue'};
         var callback = function myCallback() {};
         var context = new SarahActionContext(data, callback);
